c3bb5cef27484f7da2f358430d61a7e1341a7ba1
RosePurvis <54874990+RosePurvis@users.noreply.github.com>
2019-09-07 15:29:01 -0400

    Create Main.rb

---
+++ Main.rb
@@ -0,0 +1,15 @@
+=begin
+This will be the main program for my Coding Challenge
+The challenge is outlined in the README file, and can be referenced there. 
+=end
+
+=begin
+After completing a Ruby beginner course, I am going to make twitter using classes, methods, etc. 
+I will layout everything in comments, then begin adding in the code. 
+=end
+
+=begin
+create the class for an account, asking for parameters of username and password
+create a way for user to make a post-to be flushed out later
+create way for user to follow another user-to be flushed out later
+=end
