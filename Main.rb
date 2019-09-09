7d2cb03d429c7dcd54954edf5624105cb6f705dc
RosePurvis <54874990+RosePurvis@users.noreply.github.com>
2019-09-09 14:44:28 -0400

    Update Main.rb
    Cleaned up comments and extra variables.

--- Main.rb
+++ Main.rb
@@ -3,22 +3,21 @@ Writer: Rosalie Purvis
 Date Started: 9/7/2019
 Name: Main.rb
 
-This will be the main program for my Coding Challenge
-The challenge is outlined in the README file, and can be referenced there. 
+This will be the main program for my Coding assignment
+The assignment is outlined in the README file, and can be referenced there. 
 =end
 
 =begin
 create the class for an account, asking for parameters of username and password
-create a way for user to make a post-to be flushed out later
-create way for user to follow another user-to be flushed out later
-will not be creating a clickable user interface, instead will simply do line prompts
+create methods for user to write a post, follow other users, and see a newsfeed
+will not be creating a clickable user interface, instead will simply do line prompts, 
+next step would be to create more pleasing user interface.
 =end
 
 #Create the account Class
 class Account
    #create the users hash as global so it can be accessed from anywhere in the program
-   $users=Hash.new
-   
+   $users=Hash.new 
    #create the posts hash to hold all posts, later I can sort out the ones just for my followed users
    $posts=Hash.new
    #create a has just for followed users
@@ -37,14 +36,10 @@ class Account
   def password_error
     return "Incorrect Password"
   end 
+   
   #create the method for handling incorrect username
   def username_error
-     #if @username == username
-        
-     #else
       return "Incorrect Username"
-    # end
-     
   end
   
   #create method for seeing all users
@@ -68,62 +63,55 @@ class Account
   
   #create method to view followed user's posts
    def display_followed_posts
-      #most_recent = lambda{|x| x < Time.now}
       $followed_posts=$posts.select{$followed_users}
       return $followed_posts #ideally this is in chronological order, next step is to research how this would be handled
    end
 end
-#set variable for sign in loop to not
-
-current = false
+#In here is where in the next step I would initate a loop to guarantee that the user has actually signed in before progressing
 
 #first step is ask a person to sign up or log in!
 puts "Would you like to sign up or log in? "
 input =gets.chomp.downcase
 
 if input == "sign up"
-#create an account
-
-#ask for desired user name
-puts "Enter desired username: "
-#take the given input and assign it to username hash
-new_user=gets.chomp
-#get desired password
-puts "Enter desired password: "
-new_password = gets.chomp
-
-
-#add new user to users hash in account class, initialize the class
-current_account = Account.new(new_user,new_password)
-puts "Welcome, #{new_user}!"
+   #create an account
+   #ask for desired user name
+   puts "Enter desired username: "
+   #take the given input and assign it to username hash
+   new_user=gets.chomp
+   #get desired password
+   puts "Enter desired password: "
+   new_password = gets.chomp
 
 
-
-
+   #add new user to users hash in account class, initialize the class
+   current_account = Account.new(new_user,new_password)
+   puts "Welcome, #{new_user}!"
 else
 
-#get user input for signing in
-#need a way to initialize the class (or maybe at this point I need two different classes/ a different approach to this.)
-puts "Enter Username: "
-entered_username =gets.chomp
-#get password
-puts "Enter password: "
-entered_password = gets.chomp
-#check for account
-current_account=Account.new(entered_username,entered_password)
-if $users.keys == entered_username #having problems getting the users hash pulled, next step to investigate correct use of calling
-    if $users.values == entered_password
-         puts "You're Logged in!"
-      else
-        current_account.password_error
-      end
-   
-else 
+   #get user input for signing in
+   #need a way to initialize the class (or maybe at this point I need two different classes/ a different approach to this.)
+   puts "Enter Username: "
+   entered_username =gets.chomp
+   #get password
+   puts "Enter password: "
+   entered_password = gets.chomp
+   #check for account
+   current_account=Account.new(entered_username,entered_password)
+   if $users.keys == entered_username #having problems getting the users hash pulled, next step to investigate correct use of calling
+       if $users.values == entered_password
+            puts "You're Logged in!"
+       else
+           current_account.password_error
+       end
+
+   else 
      current_account.username_error
-end
+   end
 end
 
-#make an arbitrary loop for the program to keep running 
+#make an arbitrary loop for the program to keep running, next step would be to have this loop more like an until statement 
+#and use logging out as the way out of the loop. 
 30.times {
 #now that they are logged in, need to ask what action they would like to do!
 puts "What would you like to do? write post, follow user, see newsfeed?"
@@ -133,22 +121,17 @@ case action
   	puts "What would you like to post?: "
     post = gets.chomp
     current_account.write_post(post)  #This worked! So, I am definitely having a problem with initializing with a "sign in" instead of a "sign up"
-  
-  
   when "follow user"
   	puts "Give username: "
-  name =gets.chomp
-  if $users[name].nil?
-    puts "This user doesnt exist"
-  else 
-    current_account.follow_user(name)
-    puts "You are now following #{name}!"
-  end
-  
+   name =gets.chomp
+   if $users[name].nil?
+       puts "This user doesnt exist"
+   else 
+       current_account.follow_user(name)
+       puts "You are now following #{name}!"
+   end
   when "see newsfeed"
   	current_account.display_followed_posts
  
-  
 end
-
 }
